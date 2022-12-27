e4fd8bf56f6172b1678e172e30463c96cee2fba9
KobiReline <Kobi@Reline.Co.IL>
2022-12-27 15:11:11 +0200

    Init
    Init

---
+++ WhitelistTheAteraAgentWindowsDefender.ps1
@@ -0,0 +1,4 @@
+﻿##https://support.atera.com/hc/en-us/articles/6658561781788-Whitelist-Atera-in-Windows-Defender
+Add-MpPreference -ExclusionPath "C:\Program Files\Atera Networks\AteraAgent" -Force
+Add-MpPreference -ExclusionPath "C:\Program Files (x86)\Atera Networks\AteraAgent" -Force
+Add-MpPreference -ExclusionProcess "AteraAgent.exe" -Force
